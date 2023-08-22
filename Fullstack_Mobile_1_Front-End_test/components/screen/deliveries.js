fc1fa0fb632fc4ae5027127f73bd4453d6116bc1
Aromanda <aromandanyfitiaratoejanahary@yahoo.fr>
2023-08-22 16:49:28 -0400

    adjust delivery page

--- Fullstack_Mobile_1_Front-End_test/components/screen/deliveries.js
+++ Fullstack_Mobile_1_Front-End_test/components/screen/deliveries.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native'; // Import StyleSheet
 import CustomNavbar from '../../components/shared/header';
 import FooterNavbar from '../../components/shared/footer';
 
 const Deliveries = () => {
   // Fetch and display courier deliveries
   return (
-    <View style={{ flex: 1 }}>
+    <View style={styles.container}>
       <CustomNavbar />
-      <View>
+      <View style={styles.content}>
         <Text>Courier Deliveries</Text>
         {/* Display courier deliveries */}
       </View>
@@ -17,4 +17,14 @@ const Deliveries = () => {
   );
 };
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#F8F8F8',
+    },
+    content: {
+        flex: 1,
+        padding: 15,
+    },
+});
 export default Deliveries;
